c91218a015629b9e4d35d63175a995c4860e30b7
zander kennedy <zander kennedy>
2016-08-11 18:48:12 -0700

    Test message

---
+++ LEDSOS.js
@@ -0,0 +1,37 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    arduino: { adaptor: 'firmata', port: 'COM3' }
+  },
+
+  devices: {
+    led: { driver: 'led', pin: 13 }
+  },
+
+  work: function(my) {
+setTimeout(function() { letterS(); }, 1000);
+setTimeout(function() { letterO(); }, 4000);
+setTimeout(function() { letterS(); }, 8500);
+
+    function letterS () {
+      after((.5).second(), my.led.toggle);
+      after((1.0).second(), my.led.toggle);
+      after((1.5).second(), my.led.toggle);
+      after((2.0).second(), my.led.toggle);
+      after((2.5).second(), my.led.toggle);
+      after((3.0).second(), my.led.toggle);
+    };
+
+    function letterO () {
+      after((.5).second(), my.led.toggle);
+      after((1.5).second(), my.led.toggle);
+      after((2).second(), my.led.toggle);
+      after((3).second(), my.led.toggle);
+      after((3.5).second(), my.led.toggle);
+      after((4.5).second(), my.led.toggle);
+    }
+
+  }
+
+}).start();
